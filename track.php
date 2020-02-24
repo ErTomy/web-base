b3bd85d6d446d5de78961d572b0ae82793197d80
ertomy_madrid <ertomy@gmail.com>
2020-02-24 16:57:27 +0100

    añadido al administrador comentarios y tracks

--- track.php
+++ track.php
@@ -2,3 +2,24 @@
 if(isset($_POST['track'])){
     file_put_contents('tracks/'.$_POST['track'], date("d/m/Y H:i:s") . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
+
+/* datos de ejemplo para estadisticas 
+for ($i=0; $i < rand(100, 1000); $i++) { 
+    $randomDate = date("d/m/Y H:i:s", mt_rand(strtotime("10 September 2018"), strtotime("24 February 2020")));    
+    file_put_contents('tracks/track cinco', $randomDate . PHP_EOL, FILE_APPEND | LOCK_EX);
+}
+for ($i=0; $i < rand(100, 1000); $i++) { 
+    $randomDate = date("d/m/Y H:i:s", mt_rand(strtotime("10 September 2018"), strtotime("24 February 2020")));    
+    file_put_contents('tracks/track seis', $randomDate . PHP_EOL, FILE_APPEND | LOCK_EX);
+}
+for ($i=0; $i < rand(100, 1000); $i++) { 
+    $randomDate = date("d/m/Y H:i:s", mt_rand(strtotime("10 September 2018"), strtotime("24 February 2019")));    
+    file_put_contents('tracks/track siete', $randomDate . PHP_EOL, FILE_APPEND | LOCK_EX);
+}
+for ($i=0; $i < rand(100, 1000); $i++) { 
+    $randomDate = date("d/m/Y H:i:s", mt_rand(strtotime("10 September 2018"), strtotime("24 February 2019")));    
+    file_put_contents('tracks/track ocho', $randomDate . PHP_EOL, FILE_APPEND | LOCK_EX);
+}
+*/
+
+ 
